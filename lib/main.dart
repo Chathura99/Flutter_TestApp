49b00c4cc495a0522839ae5fe4e08f8ef9148f0b
Chathura <chathura.madura11@gmail.com>
2022-06-19 01:07:15 +0530

    container,tapbar,safeare,icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
-// ignore_for_file: prefer_const_constructors
-
-import 'dart:html';
+// ignore_for_file: prefer_const_constructors, sort_child_properties_last, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 
@@ -14,9 +12,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Hello",
       home: MyHomePage(),
-      theme: ThemeData(primarySwatch: Colors.amber),
+      // theme: ThemeData(primarySwatch: Colors.amber),
     );
   }
 }
@@ -26,14 +25,102 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Student Management",
+      // SafeArea(
+      return DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(
+              "Student Management",
+            ),
+            leading: IconButton(
+              icon: Icon(Icons.menu),
+              onPressed: () {
+                //call method in here
+              },
+            ),
+            actions: [
+              IconButton(
+                icon: Icon(Icons.search),
+                onPressed: () {
+                  //call method
+                },
+              ),
+              IconButton(
+                icon: Icon(Icons.more_vert),
+                onPressed: () {
+                  //call method
+                },
+              ),
+            ],
+            flexibleSpace: Image.asset(
+              "assets/me1.png",
+              fit: BoxFit.cover,
+            ),
+            bottom: PreferredSize(
+              child: TabBar(
+                
+                tabs: [
+                  Tab(
+                    icon: Icon(Icons.add),
+                    // text: "New",
+                    ),
+                  Tab(
+                    icon: Icon(Icons.remove),
+                    // text: "Remove",
+                    ),
+                  Tab(
+                    icon: Icon(Icons.update),
+                    // text: "Update",
+                    ),
+                  
+                ], 
+              ),
+              
+              preferredSize: Size.fromHeight(50),
+            ),
+          //  backgroundColor: Colors.pinkAccent,
+          ),
+          body: Padding(
+            padding: EdgeInsets.all(20.0),
+            child: TabBarView(
+              children: [
+                tab1(),
+                Container(
+                  color: Color.fromARGB(255, 244, 77, 133),
+                  child:Text(
+                    "Remove Student"
+                  )
+                ),
+                Text("Update Student Details"),
+              ],
+            ),
+          ),
         ),
-       
-      ), 
-  
-    );
+      );
+    // );
   }
 }
+
+Widget tab1() {
+  return Center(
+    child: Text("Add new Student"),
+  );
+}
+
+// learning outcome
+
+//stl -> StatelessWidget 
+//actions: [] -> widget array
+//child -> only one widget
+//IconButton
+//Safearea ->without curve
+//add image -> add to folder after that add to pubspec.yaml
+
+// Padding(
+//     padding: EdgeInsets.all(16.0),
+//     child: Image.asset("assets/me1.png"),
+// ),
+
+// bottom -> Tapbar
+//add TabBarView after Appbar
